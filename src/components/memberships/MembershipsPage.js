7a2ac61b78a4f3ecd41e91995243cedb0a80cf6c
Dariusz Musielak <dariusz.musielak@netguru.co>
2017-04-11 14:11:54 +0200

    Added btn for coping all emails

--- src/components/memberships/MembershipsPage.js
+++ src/components/memberships/MembershipsPage.js
@@ -10,26 +10,58 @@ class MembershipsPage extends React.Component {
   componentDidMount() {
     this.props.loadMemberships(this.props.user_email, this.props.f2f_date)
   }
+
+  copyEmails = () => {
+    this.input.select();
+    document.execCommand('copy');
+    this.input.blur();
+  }
+
+  cardHeight = (elements) => {
+    const levelsCount = Math.ceil(elements.length / 3);
+    const firstLevelHeight = 284;
+    const nextLevelHeight = 102;
+    if (levelsCount < 1 ) {
+      return firstLevelHeight;
+    }else{
+      return firstLevelHeight + (levelsCount-1) * nextLevelHeight
+    }
+  }
+
   render() {
     const groups = {};
-    this.props.memberships.forEach(membership => {
+    const { memberships } = this.props;
+    memberships.forEach(membership => {
       groups[membership.user_role] = groups[membership.user_role] || []
       groups[membership.user_role].push(membership)
     });
-
-    const cardHeight = (elements) => {
-      const levelsCount = Math.ceil(elements.length / 3);
-      const firstLevelHeight = 282;
-      const nextLevelHeight = 102;
-      if (levelsCount < 1 ) {
-        return firstLevelHeight;
-      }else{
-        return firstLevelHeight + (levelsCount-1) * nextLevelHeight
-      }
-    }
-
+    const emails = memberships.map(m => m.user_email).join(', ')
     const { quartInOut } = easings;
     const { enter, entered, exit } = enterExitStyle.fromLeftToRight;
+
+    const grid = () => {
+      return <CSSGrid
+        component="ul"
+        columns={3}
+        columnWidth={374}
+        gutterWidth={5}
+        gutterHeight={5}
+        layout={layout.pinterest}
+        duration={800}
+        easing={quartInOut}
+        enter={enter}
+        entered={entered}
+        exit={exit}
+      >
+        {Object.keys(groups).map(roleName => {
+          return (
+            <li key={roleName} itemHeight={this.cardHeight(groups[roleName])}>
+              <MembershipList memberships={groups[roleName]} roleName={roleName} />
+            </li>
+          )
+        })}
+      </CSSGrid>
+    }
 
     return (
       <Row>
@@ -37,7 +69,7 @@ class MembershipsPage extends React.Component {
           <hr/>
           <Row>
             <Col xs="10">
-              <MembershipsFiltersPage />
+              <MembershipsFiltersPage memberships={memberships} />
             </Col>
             <Col xs="2">
               <Button
@@ -51,32 +83,24 @@ class MembershipsPage extends React.Component {
           </Row>
           <hr/>
         </Col>
-          <div className="css-grid">
-            <CSSGrid
-              component="ul"
-              columns={3}
-              columnWidth={370}
-              gutterWidth={5}
-              gutterHeight={5}
-              layout={layout.pinterest}
-              duration={800}
-              easing={quartInOut}
-              enter={enter}
-              entered={entered}
-              exit={exit}
-            >
-              {Object.keys(groups).map(roleName => {
-                return (
-                  <li key={roleName} itemHeight={cardHeight(groups[roleName])}>
-                    <MembershipList memberships={groups[roleName]} roleName={roleName} />
-                  </li>
-                )
-              })}
-            </CSSGrid>
-          </div>
-        <div className="col-md-8">
-          {this.props.children}
+        <div className="css-grid">
+          {grid()}
         </div>
+        <Col xs="12">
+          <Button
+            className="btn-full-width"
+            onClick={this.copyEmails}
+            color="secondary"
+          >
+            Click to copy ALL emails
+          </Button>
+        </Col>
+        <input
+          className='push-behind-screen'
+          value={emails}
+          ref={(input) => this.input = input }
+          readOnly
+        />
       </Row>
     );
   }
