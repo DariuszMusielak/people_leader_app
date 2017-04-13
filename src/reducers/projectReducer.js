bcfbbce872bbd2bdb3965078574dae2062b71f3e
Dariusz Musielak <dariusz.musielak@netguru.co>
2017-04-13 15:07:50 +0200

    Small refactor in project and membership reducers

--- src/reducers/projectReducer.js
+++ src/reducers/projectReducer.js
@@ -1,9 +1,9 @@
-import * as types from '../actions/actionTypes';
+import { TOGGLE_PROJECT, LOAD_PROJECTS_SUCCESS } from '../actions/actionTypes';
 import initialState from './initialState';
 
 const project = (state = {}, action) => {
   switch(action.type) {
-    case types.TOGGLE_PROJECT:
+    case TOGGLE_PROJECT:
       if (state.name !== action.project.name) { return state }
 
       return {
@@ -17,9 +17,9 @@ const project = (state = {}, action) => {
 
 export default function projectReducer(state = initialState.projects, action) {
   switch(action.type) {
-    case types.LOAD_PROJECTS_SUCCESS:
+    case LOAD_PROJECTS_SUCCESS:
       return action.projects
-    case types.TOGGLE_PROJECT:
+    case TOGGLE_PROJECT:
       return state.map(i => project(i, action))
     default:
       return state;
