2d9bd62d68526eeab5c817f48dc079eb2761bf51
Dariusz Musielak <dariusz.musielak@netguru.co>
2017-04-20 17:50:22 +0200

    Create axiom client for people api

---
+++ src/api/apis.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+import cookie from 'react-cookie';
+
+export const peopleApi = axios.create({
+  baseURL: 'https://people.devguru.co/api/v3/',
+  timeout: 5000,
+  headers: {
+    "X-Email": cookie.load('user_email'),
+    "X-Api-Token": cookie.load('api_token'),
+  },
+});
