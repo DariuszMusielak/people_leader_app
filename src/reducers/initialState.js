3394a1802f680f070df927ca99f7a8f991a5c1d9
Dariusz Musielak <dariusz.musielak@netguru.co>
2017-04-20 18:43:39 +0200

    Added initial state for auth and loginForm

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -2,5 +2,15 @@ export default {
   memberships: [],
   projects: [],
   teams: [],
-  team_members: []
+  team_members: [],
+  auth: {
+    isAuthenticated: false,
+  },
+  loginForm: {
+    isSubmitting: false,
+    isSubmitted: false,
+    isValid: true,
+    email: '',
+    apiToken: '',
+  },
 }
