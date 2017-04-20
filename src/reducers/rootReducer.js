64aaddb8502933690427c84a9c6132a7738be1e0
Dariusz Musielak <dariusz.musielak@netguru.co>
2017-04-20 18:43:13 +0200

    Added auth reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -3,12 +3,14 @@ import memberships from './membershipReducer';
 import projects from './projectReducer';
 import teams from './teamReducer';
 import team_members from './teamMemberReducer';
+import auth from './authReducer';
 
 const rootReducer = combineReducers({
   memberships,
   projects,
   teams,
-  team_members
+  team_members,
+  auth,
 })
 
 export default rootReducer;
