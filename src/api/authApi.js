fbde758b52f5c3cd8b5455fecc208965af498377
Dariusz Musielak <dariusz.musielak@netguru.co>
2017-04-20 18:44:06 +0200

    Added api for signIn

---
+++ src/api/authApi.js
@@ -0,0 +1,9 @@
+import { peopleApi } from './apis'
+
+export const signIn = (email, apiToken) =>
+  peopleApi.get("/users/sign_in", {
+    params: {
+      email,
+      api_token: apiToken,
+    }
+  })
